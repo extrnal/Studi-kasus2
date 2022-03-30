c226f280c75e2c4c2384ba7f88fe47577c887c9b
GoldenCheese69 <101793719+GoldenCheese69@users.noreply.github.com>
2022-03-30 11:55:56 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ struct warung{ // struct
 	long harga, biaya, diskon, total, bayar, kembalian;
 	int jumlah;
   	char paket;	
-}wrg;
+}wrg[50];
 
 long tot(long h, long j){ // fungsi
 	  long b = h * j;
@@ -17,7 +17,10 @@ long tot(long h, long j){ // fungsi
 	string Menu [5] = {"A. Ayam Geprek		Rp.21000", "B. Ayam Goreng		Rp.17000", "C. Udang Goreng		Rp.19000", "D. Cumi Goreng		Rp.20000", "E. Ayam Bakar		Rp.25000" };
 
 int main(){
-	int i;
+	char pilih;
+	int i,jumlah;
+	float jarak;
+pilihan:
 	cout<<"========================"<<endl;
 	cout<<"    RUMAH MAKAN  "<<endl;
 	cout<<"========================"<<endl;
@@ -29,30 +32,39 @@ int main(){
 	}
 	cout<<endl<<endl;
 
-	cout<<"Pilih Menu [ A/B/C/D/E ] = "; cin>>wrg.paket;
-	cout<<"Jumlah          = "; cin>>wrg.jumlah;
+	cout<<"Pilih Menu [ A/B/C/D/E ] = "; cin>>wrg[50].paket;
+	cout<<"Jumlah          = "; cin>>wrg[50].jumlah;
 	
 
-switch(wrg.paket)
+switch(wrg[50].paket)
 {
 	case 'A':
     	case 'a'  :
-    		wrg.harga=21000;
+    		wrg[50].harga=21000;
    break;
    	case 'B':
     	case 'b'   :
-    		wrg.harga=17000;
+    		wrg[50].harga=17000;
     break;
     	case 'C':
     	case 'c'   :
-    		wrg.harga=19000;
+    		wrg[50].harga=19000;
     break;
     	case 'D':
     	case 'd'   :
-    		wrg.harga=20000;
+    		wrg[50].harga=20000;
     break;
     	case 'E':
     	case 'e'   :
-    		wrg.harga=25000;
+    		wrg[50].harga=25000;
     break;
   }
+	cout<<"ingin pesan lagi?(y/n)";
+	cin>>pilih;
+	if(pilih=='y'||pilih=='Y'){
+		system("cls");
+		goto pilihan;
+	}
+	return 0;
+}
+	
